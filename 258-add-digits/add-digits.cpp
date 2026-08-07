39b2f7e9a3429b6f762faeb3efc008d2f24b540d
Utkarsh Yadav <utkarshyadav221105@gmail.com>
2026-08-07 13:08:34 +0530

    Time: 3 ms (3.30%) | Memory: 8.6 MB (46.48%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int x){
+        if(x < 10){ 
+        return x;
+        }
+        int sum = 0;
+        while(x != 0){
+        int last = x % 10;
+        sum = sum + last;
+        x /= 10;
+        } 
+        x=sum;
+        return solve(x);
+    }
+    int addDigits(int num) {
+        return solve(num);
+
+
+
+    }
+         
+};
