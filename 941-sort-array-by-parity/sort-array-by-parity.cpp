8d0b08e387d29231c68cae12417dca95b0fff01c
Utkarsh Yadav <utkarshyadav221105@gmail.com>
2026-08-30 23:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (6.04%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
+    static bool cmp(int& a, int& b) { 
+        return a % 2 < b % 2; 
+    }
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int  n = nums.size();
-        int j = 0;           
-        for(int i = 0 ; i < n ;i++){
-            if(nums[i] % 2 == 0){
-                swap(nums[i],nums[j]);
-                j++;
-            }
-        }   
+        sort(nums.begin(), nums.end(), cmp);
         return nums;
     }
 };
