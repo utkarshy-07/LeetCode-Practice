6a351c27a485bb53dd3941ae954c98ad538c2394
Utkarsh Yadav <utkarshyadav221105@gmail.com>
2026-09-02 12:48:47 +0530

    Time: 4 ms (11.38%) | Memory: 17.5 MB (9.32%) - LeetSync

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     // static bool cmp(int &a, int &b){
-       
+
     // }
     vector<int> sortEvenOdd(vector<int>& nums) {
-      ///  sort(nums.begin(),nums.end(),cmp);
-        vector<int> en,on;
-        for(int i=0;i<nums.size();i++){
-            if(i%2==0)en.push_back(nums[i]);
-            else on.push_back(nums[i]);
+        ///  sort(nums.begin(),nums.end(),cmp);
+        vector<int> en, on;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0)
+                en.push_back(nums[i]);
+            else
+                on.push_back(nums[i]);
         }
-        sort(en.begin(),en.end());
-        sort(on.rbegin(),on.rend());
-        int e = 0 , o = 0;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(i % 2 == 0){
+        sort(en.begin(), en.end());
+        sort(on.rbegin(), on.rend());
+        int e = 0, o = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0) {
                 nums[i] = en[e++];
-            }else{
+            } else {
                 nums[i] = on[o++];
             }
         }
