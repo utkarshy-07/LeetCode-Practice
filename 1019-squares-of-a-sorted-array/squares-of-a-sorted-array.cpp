58bfaadd8cdc3d561d8b104a18e24e76705952ea
Utkarsh Yadav <utkarshyadav221105@gmail.com>
2026-09-18 11:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (87.25%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -3,32 +3,32 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
 
         // || in O(nlogn) ||
-        int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            nums[i] = nums[i] * nums[i];
-        }
-        sort(nums.begin(),nums.end());
-        return nums;
-    // }
-        // ||  in O(N) ||
         // int n = nums.size();
-        // vector<int> ans;
-        // int left = 0;
-        // int right = n - 1;
-        // int current = n - 1;
-        // while(left <= right){
-        //     int square1= nums[left] * nums[left];
-        //     int square2 = nums[right] * nums[right];
-        //     if(square1 <= square2){
-        //         ans.push_back(square1);
-        //         left++;
-        //     } else{
-        //         ans.push_back(square2);
-        //         right--;
-        //     }
-        //     current--;
+        // for(int i = 0 ; i < n ; i++){
+        //     nums[i] = nums[i] * nums[i];
         // }
-        // sort(ans.begin(),ans.end());
-        // return ans;
+        // sort(nums.begin(),nums.end());
+        // return nums;
+        // }
+        // ||  in O(N) ||
+        int n = nums.size();
+        vector<int> ans(n);
+        int left = 0;
+        int right = n - 1;
+        int current = n - 1;
+        while (left <= right) {
+            int square1 = nums[left] * nums[left];
+            int square2 = nums[right] * nums[right];
+            if (square1 > square2) {
+                ans[current] = square1;
+                left++;
+            } else {
+                ans[current] = square2;
+                right--;
+            }
+            current--;
+        }
+
+        return ans;
     }
 };
